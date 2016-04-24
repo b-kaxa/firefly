1f4aaef8f201ac90b703d912cb10f152a9f5c88c
b-kaxa <nisigin@gmail.com>
2016-04-24 18:19:00 +0900

    Delete some examples

--- public/index.php
+++ public/index.php
@@ -17,41 +17,4 @@ if ($receive_message->checkMessageKind('Text') && $receive_message->hasText('twi
     exit;
 };
 
-if ($receive_message->checkMessageKind('Text') && $receive_message->hasText('コンビニ')) {
-    $entity = $app->generateImage($receive_message);
-    $entity->setBothImageUrl('http://4.bp.blogspot.com/-XIuPfUGgqU4/Vtj9vC20r4I/AAAAAAAA4U0/p3Halb7w7fA/s800/convenience_store_car_jiko.png');
-    $app->sendMessage($entity->getResponseData());
-    exit;
-};
-
-if ($receive_message->checkMessageKind('Text') && $receive_message->hasText('みなさん')) {
-    $entity = $app->generateText($receive_message);
-    $entity->setText('楽しんでますか？');
-    $app->sendMessage($entity->getResponseData());
-    $entity = $app->generateText($receive_message);
-    $entity->setText('（ここで歓声）');
-    $app->sendMessage($entity->getResponseData());
-    exit;
-};
-
-if ($receive_message->checkMessageKind('Text') && $receive_message->hasText('PHP')) {
-    $entity = $app->generateText($receive_message);
-    $entity->setText('BLT');
-    $app->sendMessage($entity->getResponseData());
-
-    $entity = $app->generateImage($receive_message);
-    $entity->setBothImageUrl('http://2.bp.blogspot.com/-6iKr4j2u0CM/VXOTqi0XKvI/AAAAAAAAuCI/Vgnoc8qASSk/s800/food_slider_hamburger_sandwitch.png');
-    $app->sendMessage($entity->getResponseData());
-    exit;
-};
-
-if ($receive_message->checkMessageKind('Text') && $receive_message->hasText('LT')) {
-    $entity = $app->generateImage($receive_message);
-    $entity->setBothImageUrl('http://2.bp.blogspot.com/-ZCsTvsjxooM/U2Luwul3zDI/AAAAAAAAfwQ/No8KXBeFofI/s800/message_yoroshiku.png');
-    $app->sendMessage($entity->getResponseData());
-    $entity = $app->generateText($receive_message);
-    $entity->setText('（ここで拍手）');
-    $app->sendMessage($entity->getResponseData());
-    exit;
-};
 exit(1);
